1fa49d7e7eb09de00d9a24f7de894b07bbe396bf
Fuji, Goro <gfuji@cpan.org>
2010-11-12 23:45:35 +0900

    Add an example

--- example/mx-type.pl
+++ example/mx-type.pl
@@ -1,1 +1,10 @@
+#!perl -w
+use strict;
+use Data::Validator;
+use MouseX::Types::URI;
+use Data::Dumper;
+
+my $v = Data::Validator->new( uri => 'URI' );
+
+print Dumper($v->validate(uri => 'http://example.com/'));
 
