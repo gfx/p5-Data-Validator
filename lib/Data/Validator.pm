d89269b13563b9c486f29f8b3c79da1eb6f8f4bb
Fuji, Goro <gfuji@cpan.org>
2010-11-12 20:25:23 +0900

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a84ef5b..2110cb4 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Data::Validator
    
    +0.03 2010-11-12 20:24:25
    +    - Change error handling to report all the errors at once
    +
     0.02 2010-11-12 17:36:33
         - No functionality changes
         - Fix error messages

--- lib/Data/Validator.pm
+++ lib/Data/Validator.pm
@@ -5,7 +5,7 @@ use Mouse::Util::TypeConstraints ();
 use Mouse::Util                  ();
 use Carp                         ();
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 *_isa_tc  = \&Mouse::Util::TypeConstraints::find_or_create_isa_type_constraint;
 *_does_tc = \&Mouse::Util::TypeConstraints::find_or_create_does_type_constraint;
@@ -261,7 +261,7 @@ Data::Validator - Rule based validator on type constraint subsystem
 
 =head1 VERSION
 
-This document describes Data::Validator version 0.02.
+This document describes Data::Validator version 0.03.
 
 =head1 SYNOPSIS
 
