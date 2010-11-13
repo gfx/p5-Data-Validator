70b955bd6c41fa088e9a6485fcc02662f19cd5aa
Fuji, Goro <gfuji@cpan.org>
2010-11-13 10:12:55 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 2110cb4..deeaf17 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Data::Validator
    
    +0.04 2010-11-13 10:12:07
    +    - Default to confess(), not croak()
    +    - Add two extentions: Croak and NoThrow
    +
     0.03 2010-11-12 20:24:25
         - Change error handling to report all the errors at once

--- lib/Data/Validator.pm
+++ lib/Data/Validator.pm
@@ -5,7 +5,7 @@ use Mouse::Util::TypeConstraints ();
 use Mouse::Util                  ();
 use Carp                         ();
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 *_isa_tc  = \&Mouse::Util::TypeConstraints::find_or_create_isa_type_constraint;
 *_does_tc = \&Mouse::Util::TypeConstraints::find_or_create_does_type_constraint;
@@ -276,7 +276,7 @@ Data::Validator - Rule based validator on type constraint system
 
 =head1 VERSION
 
-This document describes Data::Validator version 0.03.
+This document describes Data::Validator version 0.04.
 
 =head1 SYNOPSIS
 
