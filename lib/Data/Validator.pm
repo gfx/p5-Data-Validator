cbdf924b4163e0f7cf7a6e2062538bdfda04cf0a
Fuji, Goro <g.psy.va@gmail.com>
2012-10-20 21:02:04 +0900

    Checking in changes prior to tagging of version 1.00.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index da18fed..bea3176 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Data::Validator
    
    +1.00 2012-10-20 20:58:51
    +    - Bump version to 1.00.
    +    - No feature changes. Just upgrade Module::Install.
    +
     0.09 2012-02-16 16:23:07
         - Add StrictSequenced role (karupanerura)

--- lib/Data/Validator.pm
+++ lib/Data/Validator.pm
@@ -5,7 +5,7 @@ use Mouse::Util::TypeConstraints ();
 use Mouse::Util                  ();
 use Carp                         ();
 
-our $VERSION = '0.09';
+our $VERSION = '1.00';
 
 *_isa_tc  = \&Mouse::Util::TypeConstraints::find_or_create_isa_type_constraint;
 *_does_tc = \&Mouse::Util::TypeConstraints::find_or_create_does_type_constraint;
@@ -277,7 +277,7 @@ Data::Validator - Rule based validator on type constraint system
 
 =head1 VERSION
 
-This document describes Data::Validator version 0.09.
+This document describes Data::Validator version 1.00.
 
 =head1 SYNOPSIS
 
